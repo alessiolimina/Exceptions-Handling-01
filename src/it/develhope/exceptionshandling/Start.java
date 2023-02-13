4a0be4df5c51b624063db6eae43fcd79d857b642
Alessio Limina <alessio.limina@gmail.com>
2023-02-13 16:23:54 +0100

    Exercise Exceptions Handling 01 with Javadoc

--- src/it/develhope/exceptionshandling/Start.java
+++ src/it/develhope/exceptionshandling/Start.java
@@ -9,6 +9,7 @@ public class Start {
 
     /**
      * Creating a method checkScore to check the score
+     * Since the main method is static, so it accepts only static methods, the method checkScore will be static
      * @param score is the score that we'll insert
      * if the score respects the conditions, the program prints a message
      * If not, the program throws an ArithmeticException, and it will print a warning message to inform the user
@@ -23,12 +24,19 @@ public class Start {
       }
     }
 
+    /**
+     * Creating a main method
+     * call the method checkScore with the given values
+     */
+
     public static void main(String[] args) {
-        checkScore(2.15);
-        checkScore(50);
-        checkScore(95.02);
-        checkScore(100.01);
+
+        System.out.println("----------Starting the program-------------");
+
+        Start.checkScore(2.15);
+        Start.checkScore(50);
+        Start.checkScore(95.02);
+        Start.checkScore(100.01);
     }
 }
 
-//todo complete, check and generate the Javadoc
